cbf14d8a4d3e6ec8cb0b4df1854541e7f13a7a53
tundeph <54335828+tundeph@users.noreply.github.com>
2021-09-03 18:37:02 +0100

    Update TweetHeader.js

--- src/TweetHeader.js
+++ src/TweetHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TweetHeader.css";
 import { Avatar, Button } from "@material-ui/core";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
@@ -6,8 +6,26 @@ import CropOriginalIcon from "@material-ui/icons/CropOriginal";
 import GifIcon from "@material-ui/icons/Gif";
 import BarChartIcon from "@material-ui/icons/BarChart";
 import SentimentSatisfiedIcon from "@material-ui/icons/SentimentSatisfied";
+import db from "./firebase";
 
 function TweetHeader() {
+  const [tweetMessage, setTweetMessage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("tweets").add({
+      avatar: "https://carpaddy.com/assets/img/testimonial-3.png ",
+      displayName: "Toluu",
+      image:
+        "https://media.giphy.com/media/ICeaRr3cDaF4EeDXj6/giphy.gif?cid=ecf05e47dca6dec1da64f94c4ffec9e47faef33401473447&rid=giphy.gif&ct=g",
+      text: tweetMessage,
+      userName: "toluu_",
+      verified: true,
+    });
+
+    setTweetMessage("");
+  };
   return (
     <div className="tweetBox">
       <div className="tweetHeader">
@@ -18,7 +36,11 @@ function TweetHeader() {
           <Avatar src="https://carpaddy.com/assets/img/testimonial-3.png" />
           <div className="tweetRight">
             <div className="tweetTextarea">
-              <textarea placeholder="What's happening?"></textarea>
+              <textarea
+                placeholder="What's happening?"
+                onChange={(e) => setTweetMessage(e.target.value)}
+                value={tweetMessage}
+              ></textarea>
             </div>
             <div className="tweetButtons">
               <div className="left">
@@ -28,7 +50,9 @@ function TweetHeader() {
                 <SentimentSatisfiedIcon />
               </div>
               <div className="right">
-                <Button className="tweetBox_tweetButton">Tweet </Button>
+                <Button className="tweetBox_tweetButton" onClick={sendTweet}>
+                  Tweet
+                </Button>
               </div>
             </div>
           </div>
